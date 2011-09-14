74552a7d36df6bced9c0f82d400820f7189c0db1
Christopher Dell <chris@tigrish.com>
2011-09-14 10:18:26 +0200

    Create the db dir for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'active_record'
 require 'translates_fields/translates_fields'
 
 root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+FileUtils.mkdir("#{root}/db") unless File.exist?("#{root}/db")
 ActiveRecord::Base.establish_connection(
   :adapter  => 'sqlite3',
   :database => "#{root}/db/test.sqlite")
